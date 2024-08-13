093d1846285a0139753a79d00017c40dc8f92d44
kevinmangukiya2112 <mangukiyakevin2112@gmail.com>
2024-08-13 06:32:51 +0530

    "create html pages and root file"

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
-const app=require('./app');
-console.warn(10+29+20)
-console.log(app.z())
+// const app=require('./app');
+// console.warn(10+29+20)
+// console.log(app.z())
 
-const k=[1,2,3,4,5,6,3,3];
-let result=k.filter((item)=>{
-    return item===3
-});
-console.log(result)
+// const k=[1,2,3,4,5,6,3,3];
+// let result=k.filter((item)=>{
+//     return item===3
+// });
+// console.log(result)
+
+const path = require('path');
+const expr = require('express');
+const express = require('express');
+
+const app = expr();
+const publicpath = path.join(__dirname,'Express/Public');
+console.log(publicpath);
+
+app.use(express.static(publicpath));
+app.listen(1003);
