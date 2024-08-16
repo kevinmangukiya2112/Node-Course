1cd761313ff7d36113f328d9c406c6f1709ca2df
kevinmangukiya2112 <mangukiyakevin2112@gmail.com>
2024-08-16 18:02:32 +0530

    "Ejs files and common file and show different content using ejs engine"

--- Ejs/Ejstry.js
+++ Ejs/Ejstry.js
@@ -8,8 +8,21 @@ const ViewsFolder = path.join(rootFolderPath,'views');
 app.set('view engine','ejs');
 app.set('views',ViewsFolder);
 
+app.get('/profile',(req,resp)=>{
+    const user = {
+        name : 'Kevin',
+        email : 'kevin@test.com',
+        city : 'bvn',
+        country : 'india',
+        skills : [
+            'php','js','css','node','sql','react','backend'
+        ]
+    };
+    resp.render(`profile`,{user});
+});
+
 app.get('',(req,resp)=>{
-    resp.render(`profile`);
+    resp.render(`login`)
 });
 
 app.listen(1005);
